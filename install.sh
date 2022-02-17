30b85af5b07da4e804939b317aa764291c0be1c8
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-17 11:21:09 +0530

    update installer

--- install.sh
+++ install.sh
@@ -9,5 +9,8 @@ fi
 
 mkdir -p "$INSTALL_DIR"
 git clone https://github.com/SiddharthShyniben/nv "$INSTALL_DIR"
+cd $INSTALL_DIR
+git branch --set-upstream-to=origin/main main
+cd -
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
