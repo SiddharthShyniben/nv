e070d1b9c11ebc1e3d42e6ccd829e3939238914a
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-17 09:52:19 +0530

    :sparkles: Add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Should work on unix-ish systems
+INSTALL_DIR="$HOME/.config/nvim/"
+
+if [ $# != 0 ]
+then
+	INSTALL_DIR=$1
+fi
+
+echo "Cloning to $INSTALL_DIR..."
+mkdir -p "$INSTALL_DIR"
+git clone https://github.com/SiddharthShyniben/nv "$INSTALL_DIR"
+echo 'Done.'
+echo ''
+
+echo 'Installing packer.nvim...'
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/stat/packer.nvim
+echo 'Done.'
+
+echo 'Installing plugins.'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+echo 'Done.'
