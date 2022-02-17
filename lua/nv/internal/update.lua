80acea5ddf8ffc95595c8a33addf63cea5e7285d
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-17 12:28:56 +0530

    update updater

--- lua/nv/internal/update.lua
+++ lua/nv/internal/update.lua
@@ -3,8 +3,10 @@ vim.cmd[[silent !cd ~/.config/nvim && git fetch origin main]]
 local moreCommits = tonumber(vim.fn.system("cd ~/.config/nvim && git rev-list HEAD...origin/main --count"))
 
 if moreCommits > 0 then
+	print(moreCommits .. " commits behind");
 	local choice = vim.fn.confirm("New Nv update detected. Install it?", "&Yes\n&no")
 	if choice == 1 then
 		vim.fn.system("cd ~/.config/nvim && git merge origin/main")
+		print('Done.')
 	end
 end
