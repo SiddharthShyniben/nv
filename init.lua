986e720f2a86d1165480555de1fc9c05293cefaa
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-17 10:11:51 +0530

    🚧 Basics

---
+++ init.lua
@@ -0,0 +1,2 @@
+require('nv.options')()
+require('nv.plugins')()
---
+++ lua/nv/plugins/install.lua
@@ -0,0 +1,16 @@
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function (use)
+	-- packer :heart: packer
+	use 'wbthomason/packer.nvim'
+
+	-- theme
+	use {
+		'folke/tokyonight.nvim',
+		config = function()
+			vim.g.tokyonight_style = "night"
+			vim.g.tokyonight_italic_functions = true
+			vim.cmd [[colorscheme tokyonight]]
+		end
+	}
+end)
