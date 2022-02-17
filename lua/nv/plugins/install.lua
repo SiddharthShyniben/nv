2b9cce29a9c5f9380329e4faa6f8df34911ad59a
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-17 10:53:17 +0530

    Few more plugins

--- lua/nv/plugins/install.lua
+++ lua/nv/plugins/install.lua
@@ -1,19 +1,63 @@
 vim.cmd [[packadd packer.nvim]]
 
-require('packer').startup(function (use)
-	-- packer :heart: packer
-	use 'wbthomason/packer.nvim'
+require('packer').startup(
+	function (use)
+		-- packer :heart: packer
+		use 'wbthomason/packer.nvim'
 
-	-- theme
-	use {
-		'folke/tokyonight.nvim',
-		config = function()
-			vim.g.tokyonight_style = 'night'
-			vim.g.tokyonight_italic_functions = true
-			vim.cmd [[colorscheme tokyonight]]
-		end
+		-- theme
+		use {
+			'folke/tokyonight.nvim',
+			config = function()
+				vim.g.tokyonight_style = 'night'
+				vim.g.tokyonight_italic_functions = true
+				vim.cmd [[colorscheme tokyonight]]
+			end
+		}
+
+		-- mappings
+		use 'svermeulen/vimpeccable'
+
+		-- ui
+		use {
+			'nvim-lualine/lualine.nvim',
+			requires = {'kyazdani42/nvim-web-devicons', opt = true},
+			config = function()
+				require('lualine').setup {
+					options = {
+						theme = 'tokyonight'
+					}
+				}
+			end
+		}
+		
+		use {
+			'kyazdani42/nvim-tree.lua',
+			requires = {
+				'kyazdani42/nvim-web-devicons', -- optional, for file icon
+			},
+			config = function() require('nvim-tree').setup {} end
+		}
+
+		use {
+			'nvim-lualine/lualine.nvim',
+			requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+			config = function()
+				require('lualine').setup({
+					options = {
+						theme = 'tokyonight'
+					}
+				})
+			end
+		}
+	end,
+	config = {
+		display = {
+			open_fn = function()
+				return require('packer.util').float({
+					style = 'minimal'
+				})
+			end
+		}
 	}
-
-	-- mappings
-	use 'svermeulen/vimpeccable'
-end)
+)
