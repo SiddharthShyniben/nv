eea8976d0f8ad8fdf69aa613cbba7e4b9a124781
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-17 10:37:05 +0530

    Update installer

--- install.sh
+++ install.sh
@@ -14,8 +14,9 @@ echo 'Done.'
 echo ''
 
 echo 'Installing packer.nvim...'
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+eval 'git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim'
+
 echo 'Done.'
 
 echo 'Installing plugins.'
