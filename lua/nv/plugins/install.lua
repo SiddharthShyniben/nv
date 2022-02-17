dd7bf9e0318e0d154ef3bffea4534ac8cd6bbfaa
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-17 10:17:51 +0530

    Quotes

--- lua/nv/plugins/install.lua
+++ lua/nv/plugins/install.lua
@@ -8,7 +8,7 @@ require('packer').startup(function (use)
 	use {
 		'folke/tokyonight.nvim',
 		config = function()
-			vim.g.tokyonight_style = "night"
+			vim.g.tokyonight_style = 'night'
 			vim.g.tokyonight_italic_functions = true
 			vim.cmd [[colorscheme tokyonight]]
 		end
